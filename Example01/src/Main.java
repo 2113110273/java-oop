e6e8c6f42772d211b576ab4321d3560fa60e7da0
Admin <Admin@D20925>
2015-05-23 14:28:37 +0700

    anhtuan

---
+++ Example01/src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		Rectangle recta = new Rectangle(3,4);
+		Rectangle rectb = new Rectangle(5,6);
+		Console.WriteLine("rect a:{0}", recta.area());
+		Console.WriteLine("rect b:{0}", rectb.area());
+	}
+
+}
