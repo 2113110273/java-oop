15814e747a5ef9a85a7b63d78e9ad5d6aa95140f
Admin <Admin@D20925>
2015-05-23 14:34:10 +0700

    cfvsdvg

---
+++ Example03/src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		Rectangle recta = new Rectangle(3,4);
+		Rectangle rectb = new Rectangle();
+		Console.WriteLine("rect a:{0}", recta.area());
+		Console.WriteLine("rect b:{0}", rectb.area());
+
+	}
+
+}
---
+++ Example03/src/Rectangle.java
@@ -0,0 +1,18 @@
+
+public class Rectangle {
+	int width;
+	int height;
+public Rectangle(){
+	width = 5;
+	height = 5;
+ }
+public Rectangle(int a, int b){
+	width = a;
+	height = b;
+}
+public int area(){
+	return(width*height);
+ }
+}
+
+
